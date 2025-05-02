bdc0e696da26272fd3e99549ba126198e45e214c
akimovdaniil <aki.daniil@gmail.com>
2025-05-02 13:00:29 +0300

    feat: add reversed edge filtering

--- backend/app/core/generate_random_flow.py
+++ backend/app/core/generate_random_flow.py
@@ -22,6 +22,16 @@ def filter_multiple_edges(edges: List[Edge]) -> List[Edge]:
             clear_edges.append(edge)
     return clear_edges
 
+def filter_reversed_edges(edges: List[Edge]) -> List[Edge]:
+    used = set()
+    clear_edges = []
+    for edge in edges:
+        if (((edge.from_, edge.to) not in used)
+                and (edge.from_, edge.to)):
+            used.add((edge.from_, edge.to))
+            clear_edges.append(edge)
+    return clear_edges
+
 def get_point_on_circle(centre_x : float, centre_y : float, radius : float, angle : float) -> Tuple[float, float]:
     x = centre_x + radius * math.cos(angle)
     y = centre_y + radius * math.sin(angle)
@@ -51,6 +61,7 @@ def generate_random_flow(n : int, m : int) -> Picture:
 
     edges = filter_loops(edges)
     edges = filter_multiple_edges(edges)
+    edges = filter_reversed_edges(edges)
 
     graph = Graph(n=n, m=len(edges), edges=edges)
     centre_x = 0
