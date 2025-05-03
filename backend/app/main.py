dcf3b9df37f172fd3683f419e531069af3a33362
akimovdaniil <aki.daniil@gmail.com>
2025-05-03 18:17:37 +0300

    fix : rename files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from app.routers import post_random_flow
+
+from app.routers import post_ford_fullkerson_visualization, post_random_flow
 
 app = FastAPI(debug=True)
 
 app.include_router(post_random_flow.router, prefix="/flow", tags=["flow"])
+app.include_router(
+    post_ford_fullkerson_visualization.router,
+    prefix="/visualization/ford-fullkerson",
+    tags=["visualization"],
+)
+
 
 @app.get("/")
 def read_root():
