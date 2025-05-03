bdc4f21472c55ef80d0db7ae1ef73c706ecb451f
maxim <fedaraumaxim@gmail.com>
2025-05-03 16:37:18 +0300

    feat: add testing support

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.routers import hello
+from app.routers import hello
 
 app = FastAPI()
 
--- app/routers/hello.py
+++ app/routers/hello.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+def get_hello() -> dict:
+    """Return hello."""
+    return {"message": "Hello from router!"}
+
+
 @router.get("/hello")
 def say_hello() -> dict:
     """Say Hello."""
-    return {"message": "Hello from router!"}
+    return get_hello()
