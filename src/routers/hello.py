8b9b7d375229036dad4e29efe070a8e6fd2893bc
maxim <fedaraumaxim@gmail.com>
2025-05-03 13:26:07 +0300

    feat: add ruff

--- src/routers/hello.py
+++ src/routers/hello.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/hello")
-def say_hello():
+def say_hello() -> dict:
+    """Say Hello."""
     return {"message": "Hello from router!"}
