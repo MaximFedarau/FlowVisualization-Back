287b4ae618373ca5b76d0a636bb8d0824ebec10c
maxim <fedaraumaxim@gmail.com>
2025-05-04 15:14:19 +0300

    fix: fix lint errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-from app.routers import hello, post_flow_visualization, post_random_flow, video_generation
+from app.routers import (
+    hello,
+    post_edmonds_karp_visualization,
+    post_ford_fullkerson_visualization,
+    post_random_flow,
+    video_generation,
+)
 
 app = FastAPI(debug=True)
 
@@ -8,8 +14,13 @@ app.include_router(post_random_flow.router, prefix="/flow", tags=["flow"])
 app.include_router(hello.router, prefix="/api")
 app.include_router(video_generation.router, prefix="/video", tags=["video"])
 app.include_router(
-    post_flow_visualization.router,
-    prefix="/visualization",
+    post_ford_fullkerson_visualization.router,
+    prefix="/ford-fullkerson",
+    tags=["visualization"],
+)
+app.include_router(
+    post_edmonds_karp_visualization.router,
+    prefix="/edmonds-karp",
     tags=["visualization"],
 )
 
--- app/routers/post_edmonds_karp_visualization.py
+++ app/routers/post_edmonds_karp_visualization.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
+
 from app.core.generate_edmonds_karp_visualization import generate_visualization
 from app.models.graph import Graph
 from app.models.visualization import Visualization
-from typing import List
 
 router = APIRouter()
 
+
 @router.post("/generate")
-def post_random_flow(graph : Graph) -> Visualization:
-    visualization = generate_visualization(graph)
-    return visualization
+def post_random_flow(graph: Graph) -> Visualization:
+    """Simulate Edmonds-Karp algorithm."""
+    return generate_visualization(graph)
