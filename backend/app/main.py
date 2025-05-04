5792fccdaec95abfd42dabf5ab067b20cdada7f3
akimovdaniil <aki.daniil@gmail.com>
2025-05-04 12:37:09 +0300

    feat: add dinic_algorithm

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 
 from app.routers import (
+    post_dinic_visualization,
     post_edmonds_karp_visualization,
     post_ford_fullkerson_visualization,
     post_random_flow,
 )
 
-# from app.routers import post_dinic_visualization
-
 app = FastAPI(debug=True)
-
 app.include_router(post_random_flow.router, prefix="/flow", tags=["flow"])
 app.include_router(
     post_ford_fullkerson_visualization.router,
@@ -19,7 +17,9 @@ app.include_router(
 app.include_router(
     post_edmonds_karp_visualization.router, prefix="/edmonds-karp", tags=["visualization"]
 )
-# app.include_router(post_dinic_visualization.router, prefix="/dinic", tags=["visualization"])
+app.include_router(
+    post_dinic_visualization.router, prefix="/dinic", tags=["visualization"]
+)
 
 
 @app.get("/")
