5b3921cbf62a356c1395604c617f63dbabb8fcc9
akimovdaniil <aki.daniil@gmail.com>
2025-05-04 15:48:53 +0300

    fix: delete trash model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,13 +1,1 @@
-from pydantic import BaseModel, Field
-from typing import List
 
-class MenuItem(BaseModel):
-    name: str
-    quantity: int = Field(..., ge=1)
-    price_per_unit: float = Field(..., ge=0)
-
-class Order(BaseModel):
-    customer_name: str
-    table_number: int = Field(..., ge=1)
-    items: List[MenuItem]
-    is_takeaway: bool
